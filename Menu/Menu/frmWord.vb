218868dc3fba5f5de933ce8048cd615d85114a8c
Eduardo Omar <eramirez@freshsoftwareconcepts.com>
2014-11-26 11:35:14 -0600

    Excel

--- Menu/Menu/frmWord.vb
+++ Menu/Menu/frmWord.vb
@@ -1,25 +1,7 @@
-﻿Public Class frmWord
+﻿Imports DevExpress.XtraRichEdit.API.Native
+Public Class frmWord
 
     Private Sub frmWord_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         RichEditControl1.CreateNewDocument()
-
-        Dim table As New DataTable
-
-        ' Create four typed columns in the DataTable.
-        table.Columns.Add("Dosage", GetType(Integer))
-        table.Columns.Add("Drug", GetType(String))
-        table.Columns.Add("Patient", GetType(String))
-        table.Columns.Add("Date", GetType(DateTime))
-
-        ' Add five rows with those columns filled in the DataTable.
-        table.Rows.Add(25, "Indocin", "David", DateTime.Now)
-        table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now)
-        table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now)
-        table.Rows.Add(21, "Combivent", "Janet", DateTime.Now)
-        table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now)
-
-        RichEditControl1.Options.MailMerge.DataSource = table
-        DataNavigator1.DataSource = table
-
     End Sub
 End Class
